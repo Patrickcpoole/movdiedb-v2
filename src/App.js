8d401ed660e1cd839a48086e2e155c1510cc9297
Patrick Poole <patrickcpoole@gmail.com>
2019-10-19 17:20:47 -0600

    Connected API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [movies, setMovies] = useState([]);
+	const [errors, setErrors] = useState([]);
+
+	async function fetchData() {
+		const res = await fetch(
+			'https://api.themoviedb.org/3/discover/movie?api_key=d5bf760baf0734860cc8c0f30c24aad5&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
+		);
+		res
+			.json()
+			.then(res => setMovies(res))
+			.catch(err => setErrors(err));
+	}
+
+	useEffect(() => {
+		fetchData();
+	});
+
+	return (
+		<div className='App'>
+			<p>Some text</p>
+		</div>
+	);
+};
 
 export default App;
